ea88d75e88a1f39503f2ca5b00b53db4b0f85110
Mostafa Mohamed <109727053+mostafa569@users.noreply.github.com>
2023-05-12 23:52:54 +0300

    Update adminPages.js

--- routes/adminPages.js
+++ routes/adminPages.js
@@ -173,32 +173,6 @@ router.post('/:id/add-student', isAdmin, async (req, res) => {
 
 
 
-function generateUserName(type) {
-    let result = '';
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const charactersLength = characters.length;
-    for (let i = 0; i < 5; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    if (type == 'Doctor') {
-        return 200 + result;
-    }
-    else {
-        return 300 + result;
-    }
-}
-
-
-function generateRandomPassword(nationalid) {
-    const charset = nationalid + "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let password = "";
-    for (let i = 0; i < 8; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
-    }
-    return password;
-}
-
 
 
 module.exports = router;
