193265ade1fec2450ccd991a6c21a581c2041c0e
menna samy <117574509+menni88@users.noreply.github.com>
2023-05-23 16:45:10 +0300

    Add files via upload

---
+++ routes/doctorPages.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const log = require('../config/auth');
+const isDoctor = log.isDoctor;
+const controller = require('../controllers/doctorController');
+
+
+
+
+router.get('/:id', isDoctor, controller.getDoctorHome);
+
+
+router.get('/:id/:course',isDoctor,controller.getCourseFiles);
+
+
+
+
+
+
+
+router.get('/:id/:course/:name',isDoctor,controller.getFile);
+
+
+router.post('/:id/:course/upload-pdf', isDoctor, controller.upload.array('myFiles'),controller.uploadPdf);
+
+
+
+router.post('/:id/:course/upload-scores', isDoctor, controller.UPLOAD.single('file'),controller.uploadScores);
+
+
+
+
+
+module.exports = router;
